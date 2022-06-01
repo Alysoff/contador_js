8df0ef13d5b3a0cef5578db94341d1852b1612ce
Alysoff <alysonferreirasales@gmail.com>
2022-06-01 11:21:44 -0300

    commit on 'master'

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,10 +4,20 @@ var currentNumber = 0;
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    mudaCor();
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    mudaCor();
 }
 
+// deixa os numeros negativos vermelhos
+function mudaCor(){
+    if (currentNumber < 0){
+        document.getElementById("currentNumber").style.color = 'red';
+    } else {
+        document.getElementById("currentNumber").style.color = 'black';
+    }
+}
